3ad8e1d34c1b8de76acfc5c4af1954686e5af01d
CalledCracki <calledcracki@gmail.com>
2025-10-16 12:27:46 +0200

    Added DiscordRPC

--- src/client/java/com/example/OPMODClient.java
+++ src/client/java/com/example/OPMODClient.java
@@ -1,13 +1,21 @@
 package com.example;
 
 import com.example.commands.TrackerCommand;
+import com.example.misc.PresenceBuilder;
 import com.example.ui.HUDOverlay;
+import com.jagrosh.discordipc.exceptions.NoDiscordClientException;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
 public class OPMODClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        try {
+            PresenceBuilder.start();
+        } catch (NoDiscordClientException e) {
+            throw new RuntimeException(e);
+        }
+
         HUDOverlay.init();
 
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
